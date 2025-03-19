43c5a86982157eb97c68ae94f763a2cb6b1f988b
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-19 08:48:22 -0400

    before nLify

---
+++ src/components/payment-form/payment-form.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PaymentFormContainer = styled.div`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  algign-items: center;
+  justify-content: center;
+`;
+
+export const FormConainer = styled.form`
+  height: 100px;
+  min-width: 500px;
+`;
