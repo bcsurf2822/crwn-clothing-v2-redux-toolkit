c7d69f97a99066caf472070fc164c670d6e2f94a
Benjamin Corbett <benjamincorbett@Benjamins-iMac.local>
2025-03-19 09:34:27 -0400

    finsish setting up payment success

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -7,16 +7,48 @@ export default function PaymentForm() {
   const elements = useElements();
 
   const paymentHandler = async (e) => {
-    e.preventDefaule();
+    e.preventDefault();
 
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+    // Get Client Secret form response
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    console.log("C Secret", client_secret);
+  
+  
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Ben Corbett",
+        },
+      },
+    });
+
+    if(paymentResult.error) {
+      alert(paymentResult.error) 
+    } else {
+      if(paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Success')
+      }
+    }
   };
+
   return (
     <div>
       <PaymentFormContainer>
-        <FormConainer>
+        <FormConainer onSubmit={paymentHandler}>
           <h2>Credit Card Payment: </h2> <CardElement />
           <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
         </FormConainer>
